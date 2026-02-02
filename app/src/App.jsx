020f2a1f9475bdcdefa4e7a996f6bab3a6287c1a
Dinurakshan Ravichandran <dinurakshan@gmail.com>
2026-02-02 19:25:55 +0530

    Set up tailwind

---
+++ app/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-red-600">
+      <h1 className="text-white text-5xl font-bold">TAILWIND WORKING</h1>
+    </div>
+  );
+}

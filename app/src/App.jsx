1a024aa4fde3ea1f11ed7c9e469815450b8e477d
Dinurakshan Ravichandran <dinurakshan@gmail.com>
2026-02-04 17:34:01 +0530

    structure

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage";
+import TeamPage from "./pages/team";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/team" element={<TeamPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ app/src/pages/team.jsx
@@ -0,0 +1,36 @@
+import TeamHero from "../components/teamhero";
+import TeamMember from "../components/team-members"
+import OperatingDNA from "../components/team-operatingdna";
+import GlobalFootprint from "../components/team-globalfootprint";
+import TeamCTA from "../components/team-cta";
+
+import React from "react";
+
+import TeamHero from "../components/teamhero";
+import TeamMember from "../components/team-members";
+import OperatingDNA from "../components/team-operatingdna";
+import GlobalFootprint from "../components/team-globalfootprint";
+import TeamCTA from "../components/team-cta";
+
+const Team = () => {
+  return (
+    <main className="bg-[#030210] text-white">
+      {/* Hero / Positioning */}
+      <TeamHero />
+
+      {/* Leadership Sections (one per partner) */}
+      <TeamMember />
+
+      {/* Operating DNA */}
+      <OperatingDNA />
+
+      {/* Global Footprint */}
+      <GlobalFootprint />
+
+      {/* CTA */}
+      <TeamCTA />
+    </main>
+  );
+};
+
+export default Team;

42884a2db782e1e5dbc6dcf8b739289ae89932e4
Dinurakshan Ravichandran <dinurakshan@gmail.com>
2026-02-04 22:31:48 +0530

    team change

--- app/src/pages/team.jsx
+++ app/src/pages/team.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/header"
+import Header from "../components/header";
 import TeamHero from "../components/teamhero";
 import TeamMember from "../components/team-members";
 import OperatingDNA from "../components/team-operatingdna";
